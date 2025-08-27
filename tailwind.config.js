73a2758849d1d9aa1c47f207919884085037824b
Nurul Izzah <mephistomum@gmail.com>
2025-08-27 17:23:46 +0800

    update back button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./*.html",                // root HTML files
+    "./assets/**/*.js",        // all JS files inside assets
+    "./assets/**/*.html"       // HTML files inside assets (if any)
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
